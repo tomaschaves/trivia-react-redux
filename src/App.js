5998d71069a40bb74ae9a220538b682852b9a0c2
LuanaMayumi <luh.mayumi@gmail.com>
2023-02-09 16:19:52 -0300

    requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import logo from './trivia.png';
+// import logo from './trivia.png';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={ logo } className="App-logo" alt="logo" />
+    //     <p>SUA VEZ</p>
+    //   </header>
+    // </div>
+    <Switch>
+      <Route path="/" component={ Login } />
+    </Switch>
   );
 }
