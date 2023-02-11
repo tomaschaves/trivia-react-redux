91a482b8fe1c46773bf1eb0265e45f1f0c53e1f1
LuanaMayumi <luh.mayumi@gmail.com>
2023-02-11 18:14:59 -0300

    terminando requisito 6

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -7,46 +7,46 @@ class Questions extends Component {
     indexQuestion: 0,
   };
 
-  handleClick = (xablau) => {
-    console.log(xablau);
-  };
+  // handleClick = (xablau) => {
+  //   console.log(xablau);
+  // };
 
   render() {
     const { indexQuestion } = this.state;
-    const { questions, disable, onClick } = this.props;
-    // console.log(disable);
-    // console.log(questions);
-    const {
-      correct_answer: correctAnswer,
-      incorrect_answers: incorrectAnswer,
-    } = questions[indexQuestion];
+    const { questions,
+      //  disable,
+      //   onClick
+    } = this.props;
+
+    // const {
+    //   correct_answer: correctAnswer,
+    //   incorrect_answers: incorrectAnswer,
+    // } = questions[indexQuestion];
 
     // eu preciso criar um array com todas as alternativas
-    const arrayAnswers = [...incorrectAnswer, correctAnswer];
-    // console.log(arrayAnswers);
+    // const arrayAnswers = [...incorrectAnswer, correctAnswer];
 
     // realização depois da monitoria V
     const answers = [];
 
     questions.forEach((question, index) => {
-      // console.log(question.category);
       answers.push({
         question: index,
         answer: question.correct_answer,
-        tag: 'data-testid=correct-answer',
+        // LUANA: retirei o datatestid que estava nas tags abaixo
+        tag: 'correct-answer',
       });
       question.incorrect_answers.forEach((wronganswer, indexIncorrect) => {
         answers.push({
           question: index,
           answer: wronganswer,
-          tag: `data-testid=wrong-answer-${indexIncorrect}`,
+          tag: `wrong-answer-${indexIncorrect}`,
         });
       });
     });
     // acima, temos um array de objetos que possui todas as respostas para todas as questões
 
     // FINAL DA REALIZAÇÃO PÓS MONITORIA
-    // console.log(answers);
 
     // fazer um objeto com as opções correta e incorretas
     // {
@@ -62,79 +62,76 @@ class Questions extends Component {
 
     return (
       <div>
-        <h1
-          data-testid="question-category"
-        >
-          { questions[indexQuestion].category }
+        <h1 data-testid="question-category">
+          {questions[indexQuestion].category}
         </h1>
         {/* // pega o array de objetos do estado global Questions, atribui o index, acessa a chave question */}
-        <p
-          data-testid="question-text"
-        >
-          { questions[indexQuestion].question }
-        </p>
+        <p data-testid="question-text">{questions[indexQuestion].question}</p>
         {/* REALIZAÇÃO PÓS MONITORIA */}
         {/* filtramos as respostas do array de respostas, com base no index da questão */}
-        { answers.filter((answer) => (
-          answer.question === indexQuestion
-        ))
-          .sort(() => Math.random() - MEIO)
-          .map((answer) => (
-            //       answer === questions[indexQuestion].correctAnswer
-            //         ? (
-            //           <button
-            //             key={ index }
-            //             data-testid="correct-answer"
-            //             onClick={ handleClick }
-            //             disabled={ disableButtons }
-            //           >
-            //             { answer }
-            //           </button>
-            //         )
-            //         : (
-            //           <button
-            //             key={ index }
-            //             data-testid={ `wrong-answer-${index}` }
-            //             onClick={ handleClick }
-            //             disabled={ disableButtons }
-            //           >
-            //             {' '}
-            //             { answer }
-            //             {' '}
+        {/* LUANA: div, com o data test id */}
+        <div data-testid="answer-options">
+          {answers
+          // filtra o array de respostas que esta ligado a pergunta do indexQuestion
+            .filter((answer) => answer.question === indexQuestion)
+            .sort(() => Math.random() - MEIO)
+            .map((answer) => (
+              //       answer === questions[indexQuestion].correctAnswer
+              //         ? (
+              //           <button
+              //             key={ index }
+              //             data-testid="correct-answer"
+              //             onClick={ handleClick }
+              //             disabled={ disableButtons }
+              //           >
+              //             { answer }
+              //           </button>
+              //         )
+              //         : (
+              //           <button
+              //             key={ index }
+              //             data-testid={ `wrong-answer-${index}` }
+              //             onClick={ handleClick }
+              //             disabled={ disableButtons }
+              //           >
+              //             {' '}
+              //             { answer }
+              //             {' '}
 
-            //           </button>
-            //         )
-            //     ))}
-            // </div>
+              //           </button>
+              //         )
+              //     ))}
+              // </div>
 
-            // answer === questions[indexQuestion].correctAnswer
-            //   ? (
-            <button
-              key={ answer.tag }
-              data-testid={ answer.tag }
-              // por algum motivo, não está funcionando colocar as funções passadas por props para os botões, seja a de onClick ou a de disabled, conferir isso. se colocamos elas com (), elas funcionam já na renderização, mas não é o que queremos
-              // onClick={  }
-              // disabled={ disable }
-              // disabled="true"
-            >
-              { answer.answer }
-            </button>
-            // FINAL DA REALIZAÇÃO PÓS MONITORIA
-            // )
-            // : (
-            //   <button
-            //     key={ index }
-            //     data-testid={ `wrong-answer-${index}` }
-            //     onClick={ handleClick }
-            //     disabled={ disableButtons }
-            //   >
-            //     {' '}
-            //     { answer }
-            //     {' '}
+              // answer === questions[indexQuestion].correctAnswer
+              //   ? (
+              <button
+                key={ answer.tag }
+                data-testid={ answer.tag }
+                // por algum motivo, não está funcionando colocar as funções passadas por props para os botões, seja a de onClick ou a de disabled, conferir isso. se colocamos elas com (), elas funcionam já na renderização, mas não é o que queremos
+                // onClick={  }
+                // disabled={ disable }
+                // disabled="true"
+              >
+                {answer.answer}
+              </button>
+              // FINAL DA REALIZAÇÃO PÓS MONITORIA
+              // )
+              // : (
+              //   <button
+              //     key={ index }
+              //     data-testid={ `wrong-answer-${index}` }
+              //     onClick={ handleClick }
+              //     disabled={ disableButtons }
+              //   >
+              //     {' '}
+              //     { answer }
+              //     {' '}
 
-          //   </button>
-          // )
-          ))}
+              //   </button>
+              // )
+            ))}
+        </div>
       </div>
     );
   }
